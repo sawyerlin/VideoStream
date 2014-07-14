118f36aefaa178a9c0363632ba95d8100a2d15ef
sawyerlin <sawyer.lin@gmail.com>
2014-07-14 22:29:04 +0200

    add server and client

---
+++ lib/build.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+g++ videostream.cpp -o ../obj/videostream.o
+g++ server.cpp -o ../obj/server.o
+g++ client.cpp -o ../obj/client.o
---
+++ lib/videostream.cpp
@@ -0,0 +1,13 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <errno.h>
+//#include <arpa/inet.h>
+//#include <sys/socket.h>
+//#include <fcntl.h>
+#include <iostream>
+
+int main(){
+	std::cout << "test\n"; 
+	return 0;
+}
